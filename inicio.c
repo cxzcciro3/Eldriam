483bc7e34ffbd910d2f167495fd556f9231e00ae
ciro <cicerobrciro@gmail.com>
2018-08-16 00:10:00 -0300

    Update inicio.c

--- inicio.c
+++ inicio.c
@@ -70,8 +70,8 @@ int choiceSex(void){
 	while(a == 1){
 		printf("qual seu sexo?\n-1-male\n-2 -Female\n");
 		scanf("%d",&b);
-		if(b == 1){a = 0;}
-		else if(b == 2){a = 0;}
+		if(b == 1){a = 0;return 1;}
+		else if(b == 2){a = 0;return 2;}
 		else{system("cls");}
 	}
 }
@@ -81,11 +81,11 @@ int choiceClssA(void){
 	while(a == 1){
 		printf("Selecione ua classe:\n1-gerreiro\n2-arqueiro\n3-mago\n4-ladão\n5-teplario\n");
 		scanf("%d",&b);
-		if(b == 1){a = 0;}
-		else if(b == 2){a = 0;}
-		else if(b == 3){a = 0;}
-		else if(b == 4){a = 0;}
-		else if(b == 5){a = 0;}
+		if(b == 1){a = 0;return 1;}
+		else if(b == 2){a = 0;return 2;}
+		else if(b == 3){a = 0;return 3;}
+		else if(b == 4){a = 0;return 4;}
+		else if(b == 5){a = 0;return 55;}
 		else{system("cls");}
 	}
 }
@@ -95,12 +95,12 @@ int choiceTalent(void){
 	while(a == 1){
 		printf("\n");
 		scanf("%d",&b);
-		if(b == 1){a = 0;}
-		else if(b == 2){a = 0;}
-		else if(b == 3){a = 0;}
-		else if(b == 4){a = 0;}
-		else if(b == 5){a = 0;}
-		else if(b == 6){a = 0;}
+		if(b == 1){a = 0;return 1;}
+		else if(b == 2){a = 0;return 2;}
+		else if(b == 3){a = 0;return 3;}
+		else if(b == 4){a = 0;return 4;}
+		else if(b == 5){a = 0;return 5;}
+		else if(b == 6){a = 0;return 6;}
 		else{system("cls");}
 	}
 }
