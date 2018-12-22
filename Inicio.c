875680cc6ddb68247de8c04fb2ae5b6e606f3bf5
ciro <cicerobrciro@gmail.com>
2018-12-22 10:07:48 -0300

    Update Inicio.c

--- Inicio.c
+++ Inicio.c
@@ -1,4 +1,4 @@
-//Inicio 0.5
+//Inicio 0.9
   //Necessario especificar qual o sexo do perssonagem
   //Necessario especificar qual o classe do perssonagem
   //Necessario especificar qual o talento do perssonagem
@@ -59,15 +59,21 @@ void start(void){
 	printf("****************************************\n");
 	printf("Você esta começando uma nova aventura...\n");
 	printf("antes de qualquer aventura é necesario preparação.\n");
-	printf("eentão por favor me reponda...\n");
+	printf("então por favor me reponda...\n");
 	printf("*******************************************\n");
-		
+	system("cls");	
 	sex = choiceSex();
 	classe = choiceclasse();
 	talent = choiceTalent();
 	
 	permanentSave(sex, classe, talent);
 	characterSave(classe);
+	calculoDano();
+	calculoArmadura();
+	calculoVida();
+	calculoVelocidade();
+	
+	capitulo_01();
 	
 	system("pause");
 }
@@ -168,6 +174,13 @@ void characterSave(int classe){
 	}
 	
 	fclose(file);
+
+	file = fopen("Exp.txt","w");
+
+	fprintf(file,"0 0 1\n");
+
+	fclose(file);
+
 }
 void calculoDano(void){
 	int i;
@@ -182,13 +195,13 @@ void calculoDano(void){
 
 	FILE *file;
 	file = fopen("CharacterSave.txt","r");
-	fscanf(file,"%d",forca);
-	fscanf(file,"%d",lixo);
-	fscanf(file,"%d",dexteza);
-	fscanf(file,"%d",lixo);
-	fscanf(file,"%d",lixo);
-	fscanf(file,"%d",lixo);
-	fscanf(file,"%d",arma);
+	fscanf(file,"%d",&forca);
+	fscanf(file,"%d",&lixo);
+	fscanf(file,"%d",&dexteza);
+	fscanf(file,"%d",&lixo);
+	fscanf(file,"%d",&lixo);
+	fscanf(file,"%d",&lixo);
+	fscanf(file,"%d",&arma);
 	fclose(file);
 	if(arma == 1){danoArma = 30;cadencia = 50;}
 	else if(arma == 2){danoArma = 20;cadencia = 75;}
@@ -213,17 +226,19 @@ void calculoDano(void){
 	fprintf(file,"%d %d", danoMaximo, danoMinimo);
 	fclose(file);
 }
-int calculoArmadura(void){
+void calculoArmadura(void){
 	int a = 0;
 	int b = 5;
 	int lixo;
 	int forca;
 	int agilidade;
 	int constituicao;
+
 	int capacete;
 	int armadura;
 	int luvas;
 	int botas;
+
 	int capaceteV;
 	int armaduraV;
 	int luvasV;
@@ -232,128 +247,562 @@ int calculoArmadura(void){
 	int armaduraA;
 	int luvasA;
 	int botasA;
-
+	int armaduraTotal;
+	int def;
+	int velocidadeArmadura;
 	
 	FILE *file;
 	file = fopen("CharacterSave.txt","r");
+	fscanf(file,"%d",&forca);
+	fscanf(file,"%d",&agilidade);
+	fscanf(file,"%d",&lixo);
+	fscanf(file,"%d",&constituicao);
+	fscanf(file,"%d",&lixo);
+	fscanf(file,"%d",&lixo);
+
+	fscanf(file,"%d",&lixo);
+	fscanf(file,"%d",&capacete);
+	fscanf(file,"%d",&armadura);
+	fscanf(file,"%d",&luvas);
+	fscanf(file,"%d",&botas);
+	
 	
 	fclose(file);
-
+ 	capacete = capacete - 1;
+	armadura = armadura - 1;
+	luvas = luvas - 1;
+	botas = botas - 1;
 
 	file = fopen("Capacete.txt","r");
-	while(a < b){
-		fscanf(file,"%d",lixo);
-		fscanf(file,"%d",lixo);
+	while(a < capacete){
+		fscanf(file,"%d",&lixo);
+		fscanf(file,"%d",&lixo);
 		a = a + 1;
 	}
-	fscanf(file,"%d",capaceteA);
-	fscanf(file,"%d",capaceteV);
+	fscanf(file,"%d",&capaceteA);
+	fscanf(file,"%d",&capaceteV);
 	a = 0;
 	fclose(file);
 
 	file = fopen("Armadura.txt","r");
-	while(a < b){
-		fscanf(file,"%d",lixo);
-		fscanf(file,"%d",lixo);
+	while(a < armadura){
+		fscanf(file,"%d",&lixo);
+		fscanf(file,"%d",&lixo);
 		a = a + 1;
 	}
-	fscanf(file,"%d",armaduraA);
-	fscanf(file,"%d",armaduraV);
+	fscanf(file,"%d",&armaduraA);
+	fscanf(file,"%d",&armaduraV);
 	a = 0;
 	fclose(file);
 
 	file = fopen("Luvas.txt","r");
-	while(a < b){
-		fscanf(file,"%d",lixo);
-		fscanf(file,"%d",lixo);
+	while(a < luvas){
+		fscanf(file,"%d",&lixo);
+		fscanf(file,"%d",&lixo);
 		a = a + 1;
 	}
-	fscanf(file,"%d",luvasA);
-	fscanf(file,"%d",luvasV);
+	fscanf(file,"%d",&luvasA);
+	fscanf(file,"%d",&luvasV);
 	a = 0;
 	fclose(file);
 
 	file = fopen("Botas.txt","r");
-	while(a < b){
-		fscanf(file,"%d",lixo);
-		fscanf(file,"%d",lixo);
+	while(a < botas){
+		fscanf(file,"%d",&lixo);
+		fscanf(file,"%d",&lixo);
 		a = a + 1;
 	}
-	fscanf(file,"%d",botasA);
-	fscanf(file,"%d",botasV);
+	fscanf(file,"%d",&botasA);
+	fscanf(file,"%d",&botasV);
+	fclose(file);
+	armaduraTotal = capaceteA + armaduraA + luvasA + botasA;
+	velocidadeArmadura = capaceteV + armaduraV + luvasV + botasV;
+	def = armaduraTotal + constituicao;
+	file = fopen("CharacterSaveDef.txt","w");
+	fprintf(file,"%d %d", def, velocidadeArmadura);
 	fclose(file);
 }
-int calculoVida(void){
+void calculoVida(void){
+	int lixo; 
+	int constituicao;
+	int vida;
+
 	FILE *file;
 	file = fopen("CharacterSave.txt","r");
+	fscanf(file,"%d",&lixo);
+	fscanf(file,"%d",&lixo);
+	fscanf(file,"%d",&lixo);
+	fscanf(file,"%d",&constituicao);
+	fclose(file);
+	
+	vida = 10 + 5 * constituicao;	
+
+	file = fopen("VidaSave.txt","w");
+	fprintf(file,"%d", vida);
 	fclose(file);
 }
-int calculoVelocidade(void){
+void calculoVelocidade(void){
 	int a = 0;
 	int b = 5;
 	int lixo; 
-	int capaceteV;
+	int agilidade;
 	int armaduraV;
-	int luvasV;
-	int botasV; 
 	int armaV; 
+	int arma;
+	int velocidade;
 
 	FILE *file;
-	file = fopen("CharacterSave.txt","w");
+	file = fopen("CharacterSave.txt","r");
+	fscanf(file,"%d",&lixo);
+	fscanf(file,"%d",&agilidade);
+	fscanf(file,"%d",&lixo);
+	fscanf(file,"%d",&lixo);
+	fscanf(file,"%d",&lixo);
+	fscanf(file,"%d",&lixo);
+	fscanf(file,"%d",&arma);
+
 	fclose(file);
+	arma = arma - 1;
 	file = fopen("Armas.txt","r");
-	while(a < b){
-		fscanf(file,"%d",lixo);
-		fscanf(file,"%d",lixo);
-		fscanf(file,"%d",lixo);
+	while(a < arma){
+		fscanf(file,"%d",&lixo);
+		fscanf(file,"%d",&lixo);
+		fscanf(file,"%d",&lixo);
 		a = a + 1;
 	}
-	fscanf(file,"%d",lixo);
-	fscanf(file,"%d",armaV);
-	fscanf(file,"%d",lixo);
+	fscanf(file,"%d",&lixo);
+	fscanf(file,"%d",&armaV);
+	fscanf(file,"%d",&lixo);
 	a = 0;
 	fclose(file);
+	file = fopen("CharacterSaveDef.txt","r");
+	fscanf(file,"%d",&lixo);
+	fscanf(file,"%d",&armaduraV);
+	fclose(file);
+ 	velocidade = 100 + armaduraV + armaV;
+	file = fopen("CharacterSaveVel.txt","w");
+	fprintf(file,"%d", velocidade);
+	fclose(file);
+}
+
+void menu(){
+	bool mantemLoopMenu = 1;
+	int opcaoUsuarioMenu = 0;
+
+	while(mantemLoopMenu == 1){
+		printf("**********menu do jogador**********\n");
+		printf("1 - continuar\n");
+		printf("2 - inventario\n");
+		printf("3 - equipamentos\n");
+		printf("4 - personagem\n");
+		printf("5 - sair para o menu principal\n");
+		printf("***********************************\n");
+		
+		scanf("%d",&opcaoUsuarioMenu);
+		
+		if(opcaoUsuarioMenu == 1){
+			system("cls");
+			mantemLoopMenu = 0;
+		}else if(opcaoUsuarioMenu == 2){
+			inventario();
+		}else if(opcaoUsuarioMenu == 3){
+			eqipamentos();
+		}else if(opcaoUsuarioMenu == 4){
+			personagem();
+		}
+		system("cls");
+}
+
+void inventario(){
+
+	bool mantemLoopMenu = 1;
+	int opcaoUsuarioMenu = 0;
+
+	while(mantemLoopMenu == 1){
+		printf("**********Inventario**********\n");
+		printf("1 - equipamentos\n");
+		printf("2 - consumiveis\n");
+		printf("3 - itens\n");
+		printf("5 - sair\n");
+		printf("***********************************\n");
+		
+		scanf("%d",&opcaoUsuarioMenu);
+		
+		if(opcaoUsuarioMenu == 1){
+			inventarioEquipamentos();
+		}else if(opcaoUsuarioMenu == 2){
+			inventarioConsumiveis();
+		}else if(opcaoUsuarioMenu == 3){
+			inventarioItens();
+		}else if(opcaoUsuarioMenu == 4){
+			system("cls");
+			mantemLoopMenu = 0;
+		}
+		system("cls");
+}
+void eqipamentos(){
+
+}
+void personagem(){
+	bool mantemLoopMenu = 1;
+	int opcaoUsuarioMenu = 0;
+	int lixo;
+	int forca;
+	int agilidade;
+	int dexteza;
+	int constituicao;
+	int inteligencia;
+	int sabedoria;
+	int pontos;
 	
-	file = fopen("Capacete.txt","r");
-	while(a < b){
-		fscanf(file,"%d",lixo);
-		fscanf(file,"%d",lixo);
-		a = a + 1;
-	}
-	fscanf(file,"%d",lixo);
-	fscanf(file,"%d",capaceteV);
-	a = 0;
+
+	FILE *file;
+	file = fopen("CharacterSave.txt","r");
+	fscanf(file,"%d",&forca);
+	fscanf(file,"%d",&agilidade);
+	fscanf(file,"%d",&dexteza);
+	fscanf(file,"%d",&constituicao);
+	fscanf(file,"%d",&inteligencia);
+	fscanf(file,"%d",&sabedoria);
 	fclose(file);
 
-	file = fopen("Armadura.txt","r");
-	while(a < b){
-		fscanf(file,"%d",lixo);
-		fscanf(file,"%d",lixo);
-		a = a + 1;
-	}
-	fscanf(file,"%d",lixo);
-	fscanf(file,"%d",armaduraV);
-	a = 0;
+	while(mantemLoopMenu == 1){
+		printf("**********personagem**********\n");
+		printf("for: %d\n");
+		printf("agi: %d\n");
+		printf("dex: %d\n");
+		printf("com: %d\n");
+		printf("int: %d\n");
+		printf("sab: %d\n");
+		printf("ponts: %d\n");
+		printf("******************************\n");
+
+		printf("1 - Almentar estato\n");
+		printf("2 - mais informacões\n");
+		printf("3 - voltar\n");
+		printf("******************************\n");
+		
+		scanf("%d",&opcaoUsuarioMenu);
+		
+		if(opcaoUsuarioMenu == 1){
+			upStatus();
+		}else if(opcaoUsuarioMenu == 2){
+			InfomacoesDoPersonagem();
+		}else if(opcaoUsuarioMenu == 3){
+			system("cls");
+			mantemLoopMenu = 0;
+		}
+		system("cls");
+}
+
+
+void inventarioEquipamentos(){
+	bool mantemLoopMenu = 1;
+	int opcaoUsuarioMenu = 0;
+
+	while(mantemLoopMenu == 1){
+		printf("**********Inventario**********\n");
+		printf("1 - Armas\n");
+		printf("2 - Capacetes\n");
+		printf("3 - Armaduras\n");
+		printf("4 - Luvas\n");
+		printf("5 - Botas\n");
+		printf("6 - voltar\n");
+		printf("******************************\n");
+		
+		scanf("%d",&opcaoUsuarioMenu);
+		
+		if(opcaoUsuarioMenu == 1){
+			inventarioArmas();
+		}else if(opcaoUsuarioMenu == 2){
+			inventarioCapacetes();
+		}else if(opcaoUsuarioMenu == 3){
+			inventarioArmaduras();
+		}else if(opcaoUsuarioMenu == 4){
+			inventarioLuvas();
+		}else if(opcaoUsuarioMenu == 5){
+			inventarioBotas();
+		}else if(opcaoUsuarioMenu == 6){
+			system("cls");
+			mantemLoopMenu = 0;
+		}
+		system("cls");
+}
+void inventarioConsumiveis(){
+
+}
+void inventarioItens(){
+
+}
+
+
+upStatus(){
+
+	bool mantemLoopMenu = 1;
+	int opcaoUsuarioMenu = 0;
+	int lixo;
+	int forca;
+	int agilidade;
+	int dexteza;
+	int constituicao;
+	int inteligencia;
+	int sabedoria;
+	int pontos;
+
+	int arma;
+	int capacete;
+	int armadura;
+	int luvas;
+	int botas;
+
+	int forcaNova;
+	int agilidadeNova;
+	int dextezaNova;
+	int constituicaoNova;
+	int inteligenciaNova;
+	int sabedoriaNova;
+	int pontosNovos;
+	
+
+	FILE *file;
+	file = fopen("CharacterSave.txt","r");
+	fscanf(file,"%d",&forca);
+	fscanf(file,"%d",&agilidade);
+	fscanf(file,"%d",&dexteza);
+	fscanf(file,"%d",&constituicao);
+	fscanf(file,"%d",&inteligencia);
+	fscanf(file,"%d",&sabedoria);
+
+	fscanf(file,"%d",&arma);
+	fscanf(file,"%d",&capacete);
+	fscanf(file,"%d",&armadura);
+	fscanf(file,"%d",&luvas);
+	fscanf(file,"%d",&botas);
 	fclose(file);
 
-	file = fopen("Luvas.txt","r");
-	while(a < b){
-		fscanf(file,"%d",lixo);
-		fscanf(file,"%d",lixo);
-		a = a + 1;
-	}
-	fscanf(file,"%d",lixo);
-	fscanf(file,"%d",luvasV);
-	a = 0;
+	forca = forcaNova;
+	agilidade = agilidadeNova;
+	dexteza = dextezaNova;
+	constituicao = constituicaoNova;
+	inteligencia = inteligenciaNova;
+	sabedoria = sabedoriaNova;
+	pontos = pontosNovos;
+
+	while(mantemLoopMenu == 1){
+		printf("**********personagem**********\n");
+		printf("for: %d > for: %d\n",forca,forcaNova);
+		printf("agi: %d > agi: %d\n",agilidade,agilidadeNova);
+		printf("dex: %d > dex: %d\n",dexteza,dextezaNova);
+		printf("com: %d > com: %d\n",constituicao,constituicaoNova);
+		printf("int: %d > int: %d\n",inteligencia,inteligenciaNova);
+		printf("sab: %d > sab: %d\n",sabedoria,sabedoriaNova);
+		printf("ponts: %d\n",pontosNovos);
+		printf("******************************\n");
+
+		printf("1 - up for\n");
+		printf("2 - up agi\n");
+		printf("3 - up dex\n");
+		printf("4 - up com\n");
+		printf("5 - up int\n");
+		printf("6 - up sab\n");
+		printf("7 - salvar\n");
+		printf("8 - canselar\n");
+		printf("9 - voltar\n");
+		printf("******************************\n");
+		
+		scanf("%d",&opcaoUsuarioMenu);
+		
+		if(opcaoUsuarioMenu == 1){
+			if(pontosNovos > 0){
+				pontosNovos = pontosNovos - 1;
+				forcaNova = forcaNova + 1;
+			}
+		}else if(opcaoUsuarioMenu == 2){
+			if(pontosNovos > 0){
+				pontosNovos = pontosNovos - 1;
+				agilidadeNova = agilidadeNova + 1;
+			}
+		}else if(opcaoUsuarioMenu == 3){
+			if(pontosNovos > 0){
+				pontosNovos = pontosNovos - 1;
+				dextezaNova =  dextezaNova + 1;
+			}
+		}else if(opcaoUsuarioMenu == 4){
+			if(pontosNovos > 0){
+				pontosNovos = pontosNovos - 1;
+				constituicaoNova = constituicaoNova + 1;
+			}
+		}else if(opcaoUsuarioMenu == 5){
+			if(pontosNovos > 0){
+				pontosNovos = pontosNovos - 1;
+				inteligenciaNova = inteligenciaNova + 1;
+			}
+		}else if(opcaoUsuarioMenu == 6){
+			if(pontosNovos > 0){
+				pontosNovos = pontosNovos - 1;
+				sabedoriaNova = sabedoriaNova + 1;
+			}
+		}else if(opcaoUsuarioMenu == 7){
+			file = fopen("CharacterSave.txt","w");
+			
+			fprintf(file,"%d %d %d %d %d %d %d %d %d %d %d",forcaNova, agilidadeNova, dextezaNova, constituicaoNova, inteligenciaNova, sabedoriaNova, arma, capacete, armadura, luvas, botas);
+
+			fclose(file);
+			upStatus();
+			mantemLoopMenu = 0;
+		}else if(opcaoUsuarioMenu == 8){
+			upStatus();
+			mantemLoopMenu = 0;
+		}else if(opcaoUsuarioMenu == 9){
+			system("cls");
+			mantemLoopMenu = 0;
+		}
+		system("cls");
+
+}
+InfomacoesDoPersonagem(){
+	int a = 1;
+	int b;
+	int vida;
+	int dano;
+	int defesa;
+	int velocidade;
+
+	file = fopen("CharacterSave.txt","r");
+	fscanf(file,"%d",&vida);
 	fclose(file);
+	file = fopen("CharacterSave.txt","r");
+	fscanf(file,"%d",&dano);
+	fclose(file);
+	file = fopen("CharacterSave.txt","r");
+	fscanf(file,"%d",&defesa);
+	fclose(file);
+	file = fopen("CharacterSave.txt","r");
+	fscanf(file,"%d",&velocidade);
+	fclose(file);
+
+	while(a == 1){
+		printf("****************************************\n");
+		printf("vida: %d\n", vida);
+		printf("dano: %d\n", dano);
+		printf("defesa: %d\n", defesa);
+		printf("velocidaade de ataque: %d\n", velocidade);
+		printf("critico\n");
+		printf("dano critico\n");
+		printf("****************************************\n");
+		printf("****************************************\n\n\n");
+		
+		printf("****************************************\n");
+		printf("insira 1 para continuar...\n");
+		printf("****************************************\n");
+
+		scanf("%d",&b);
+
+		if(b == 1){
+			a = 0;
+		}
+	}
+
+}
+
+
+void strat(void){
+	srand(time(NULL));
+	int a = 1, b = 0, c = 1;
+	int mostroHP1 , mostroHP2, mostroHP3, mostroHP4, HP, danoEspada, danoEscudo, danomostro1, danomostro2, danomostro3, danomostro4, dano, escolha;
+	while(a == 1){
+		printf("-- \n");
+		printf("hp: %d \n",HP);
+		printf("1- espada: 4 - 10 \n");
+		printf("2- escudo 50%  2-6 \n");
+		printf(" \n");
+		scanf("%d", &b);
+		c = 1;
+		system("cls");
+		while(c == 1){
+			c = 0;
+			
+			if(b == 1){
+				danoEspada = (rand() %7) + 4;
+				dano = danoEspada;
+
+				danoBat1 =(rand() %3) + 2;
+				danoBat2 =(rand() %3) + 2;
+				
+				if(batHP1 > 0){HP = HP - danoBat1;}
+				if(batHP2 > 0){HP = HP - danoBat2;}
+				c = 0;
+			}
+
+		
+			else if(b == 2){ 
+				danoEscudo = (rand() %5) + 2;
+				dano = danoEscudo;
+
+				danoBat1 =(rand() %2) + 1;
+				danoBat2 =(rand() %2) + 1;
+
+				if(batHP1 > 0){HP = HP - danoBat1;}
+				if(batHP2 > 0){HP = HP - danoBat2;}
+				c = 0;	
+			}
+			
+
+			else{c = 1;}
+
+		}
+
+			c = 1;
+			while(c == 1){
+					
+				if(batHP1 > 0){printf("1- bat: %d \n",batHP1);}else{printf("1- bat: 0\n");}
+				if(batHP2 > 0){printf("2- bat: %d \n",batHP2);}else{printf("2- bat: 0\n");}
+				scanf("%d",&b);
+
+				danoEspada = (rand() %7) + 4;
+				if(b == 1 && batHP1 > 0){
+					escolha = 1;
+					batHP1 = batHP1 - dano;
+					c = 0;
+				}
+				else if(b == 2 && batHP2 > 0){
+					escolha = 2;
+					batHP2 = batHP2 - dano;
+					c = 0;
+				}
+				else if(batHP2 < 0 && batHP2 < 0 ){c = 0;}
+				else{system("cls");}
+			}
+
+
 	
-	file = fopen("Botas.txt","r");
-	while(a < b){
-		fscanf(file,"%d",lixo);
-		fscanf(file,"%d",lixo);
-		a = a + 1;
+		system("cls");
+		if(batHP1 > 0){printf("bat1 dano: %d \n",danoBat1);}
+		if(batHP2 > 0){printf("bat2 dano: %d \n",danoBat2);}
+		if(escolha == 1){printf("voce causou %d de na em Bat1 ele tem %d agora\n ", dano, batHP1);}
+		if(escolha == 2){printf("voce causou %d de na em Bat2 ele tem %d agora\n ", dano, batHP2);}
+		system("pause");
+		system("cls");
+
+		if(HP<1) {
+			a = 0;
+			printf("you died \n");
+		}
+		else if (batHP1 < 1 && batHP2 < 1){
+			a = 0;
+			printf("you win! \n");
+		}
+		
+		
 	}
-	fscanf(file,"%d",lixo);
-	fscanf(file,"%d",botasV);
-	fclose(file);
+
+}
+
+
+
+void capitulo_01(){
+	printf("****************************************\n");
+	printf("****************************************\n");
+	printf("Voce retoma a consiencia, sente aguas nos pes e areia na sua cara, voce se levanta lentantamnte e obiserva o  terendo em vouta .\n");
+	
 }
